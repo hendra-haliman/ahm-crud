93a27e30b2c39da414471550966967eee2b16972
Hendra Haliman <hendra.haliman@gmail.com>
2023-12-01 04:11:47 +0700

    added messages.properties

--- src/main/java/com/test/kendaraan/model/Kendaraan.java
+++ src/main/java/com/test/kendaraan/model/Kendaraan.java
@@ -12,12 +12,12 @@ import lombok.Data;
 @Entity
 @Data
 public class Kendaraan {
-    @Size(min = 1, max = 10, message = "Minimum 1 character and maximum 10 character")
-    @NotNull(message = "Cannot be empty")
+    @Size(min = 1, max = 10, message = "{kendaraan.noRegistrasi.minimum}")
+    @NotNull(message = "{kendaraan.noRegistrasi.notNull}")
     @Id
     private String noRegistrasi;
 
-    @Size(min = 1, max = 20, message = "Minimum 1 character and maximum 20 character")
+    @Size(min = 1, max = 20, message = "${kendaraan.namaPemilik.size}")
     private String namaPemilik;
 
     private String alamat;
